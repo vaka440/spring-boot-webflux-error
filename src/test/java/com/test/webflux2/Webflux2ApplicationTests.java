cfa93d3acfc10d704d5b4eb3287de3844ed255f6
dava440 <poub440@gmail.com>
2021-10-30 10:31:14 +0200

    first commit

---
+++ src/test/java/com/test/webflux2/Webflux2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.webflux2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
